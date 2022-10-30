509137a29dc719f7b1548a0a579f099e69fec32a
Ilshat Sultanov <ilshat@sultanov.team>
2022-10-31 01:55:29 +0300

    Add test runs

--- modules/hypercube/src/main.rs
+++ modules/hypercube/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
   println!("Hello, hypercube!");
 }
+
+#[allow(dead_code)]
+fn square(x: i32) -> i32 {
+  x.pow(2)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn square_test() {
+    assert_eq!(square(2), 4);
+  }
+}
